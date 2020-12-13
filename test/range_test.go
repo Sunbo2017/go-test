0806590225616ed3bf0b290b3e3454fd0d040f4a
sunbo2017 <962381354@qq.com>
2020-12-13 23:14:42 +0800

    add range test

---
+++ test/range_test.go
@@ -0,0 +1,40 @@
+package test
+
+import "testing"
+
+type student struct {
+	Name string
+	Age  int
+}
+
+// for Range遍历使用副本的方式。
+// 所以m[stu.Name]=&stu实际上一致指向同一个指针，最终该指针的值为遍历的最后一个struct的值拷贝
+func TestRangeStudent(t *testing.T) {
+	m := make(map[string]*student)
+	stus := []student{
+		{
+			Name: "zhou",
+			Age:  24,
+		}, {
+			Name: "li",
+			Age:  23,
+		}, {
+			Name: "wang",
+			Age:  22,
+		},
+	}
+	// 错误写法
+	for _, stu := range stus {
+		m[stu.Name] = &stu
+	}
+	for k, v := range m {
+		println(k, "=>", v.Name)
+	}
+	// 正确
+	for i := 0; i < len(stus); i++ {
+		m[stus[i].Name] = &stus[i]
+	}
+	for k, v := range m {
+		println(k, "=>", v.Name)
+	}
+}
--- test/stack_test.go
+++ test/stack_test.go
@@ -7,31 +7,30 @@ import (
 )
 
 type (
-
-	node struct{
-		val interface{}
+	node struct {
+		val  interface{}
 		prev *node
 	}
 
-	Stack struct{
-		top *node
+	Stack struct {
+		top    *node
 		length int
-		lock *sync.Mutex
+		lock   *sync.Mutex
 	}
 )
 
-func NewStack() *Stack{
+func NewStack() *Stack {
 	return &Stack{nil, 0, &sync.Mutex{}}
 }
 
-func (s *Stack) Push(val interface{}){
+func (s *Stack) Push(val interface{}) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	s.top = &node{val, s.top}
 	s.length++
 }
 
-func (s *Stack) Pop() interface{}{
+func (s *Stack) Pop() interface{} {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	if s.length == 0 {
@@ -43,7 +42,7 @@ func (s *Stack) Pop() interface{}{
 	return n.val
 }
 
-func (s *Stack) Peek() interface{}{
+func (s *Stack) Peek() interface{} {
 	if s.length == 0 {
 		return nil
 	}
@@ -73,35 +72,58 @@ func Benchmark_Pop(b *testing.B) {
 	}
 }
 
-// 校验字符串是否是合规括号对
+// LeetCode-cookbook-20：校验字符串是否是合规括号对
+// 使用结构体实现栈结构
 func isValid(s string) bool {
-    // 括号对字典
-    bracketsMap := map[uint8]uint8{'{': '}', '[': ']', '(': ')'}
-    // 传入字符串为空则返回true
-    if s == "" {
-        return true
-    }
-    // 初始化栈
-    stack := NewStack()
-    for i := 0; i < len(s); i++ {
-        // 如果栈中有数据，则进行比对，如果栈顶元素和当前元素匹配，则弹出，其他情况向栈中压入元素
-        if stack.Len() > 0 {
-            if c, ok := bracketsMap[stack.Peek().(uint8)]; ok && c == s[i] {
-                stack.Pop()
-                continue
-            }
-        }
-        stack.Push(s[i])
-    }
-    // 到最后如果栈不为空，则说明未完全匹配掉（完全匹配的话，栈只能为空）
-    return stack.Len() == 0
+	// 括号对字典
+	bracketsMap := map[uint8]uint8{'{': '}', '[': ']', '(': ')'}
+	// 传入字符串为空则返回true
+	if s == "" {
+		return true
+	}
+	// 初始化栈
+	stack := NewStack()
+	for i := 0; i < len(s); i++ {
+		// 如果栈中有数据，则进行比对，如果栈顶元素和当前元素匹配，则弹出，其他情况向栈中压入元素
+		if stack.Len() > 0 {
+			if c, ok := bracketsMap[stack.Peek().(uint8)]; ok && c == s[i] {
+				stack.Pop()
+				continue
+			}
+		}
+		stack.Push(s[i])
+	}
+	// 到最后如果栈不为空，则说明未完全匹配掉（完全匹配的话，栈只能为空）
+	return stack.Len() == 0
 }
 
-func TestValid(t *testing.T){
+// LeetCode-cookbook-20：校验字符串是否是合规括号对
+// 直接使用切片实现栈结构
+func isValidSample(s string) bool {
+	// 括号对字典
+	bracketsMap := map[rune]rune{'{': '}', '[': ']', '(': ')'}
+	// 空字符串直接返回 true
+	if len(s) == 0 {
+		return true
+	}
+	stack := make([]rune, 0)
+	for _, v := range s {
+		if (v == '[') || (v == '(') || (v == '{') {
+			stack = append(stack, v)
+		} else if c, ok := bracketsMap[stack[len(stack)-1]]; ok && c == v && len(stack) > 0 {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
+
+func TestValid(t *testing.T) {
 	s1 := "()[]{}"
 	s2 := "([]){}"
 	s3 := "([)]{}"
 	fmt.Println(isValid(s1))
-	fmt.Println(isValid(s2))
+	fmt.Println(isValidSample(s2))
 	fmt.Println(isValid(s3))
 }
