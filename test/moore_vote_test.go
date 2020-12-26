fbbbe5fad5683b4701beb72fe0053781eae3380a
sunbo2017 <962381354@qq.com>
2020-12-26 21:44:37 +0800

    over moore vote

--- test/moore_vote_test.go
+++ test/moore_vote_test.go
@@ -1,6 +1,14 @@
 package test
 
-// 摩尔投票算法是用于找出数组中出现次数超过n/2的元素，
+import (
+	"fmt"
+	"testing"
+)
+
+// 摩尔投票算法是用于找出数组中出现次数超过n/k的元素
+
+
+// 若想找出数组中出现次数超过n/2的元素，
 // 其核心思想在于遍历过程中不同元素之间两两抵消，
 // 由于一个数组中，出现次数超过n/2最多只有一个，
 // 那么遍历结束时，未被抵消掉的即是出现次数超过n/2的元素。
@@ -10,7 +18,7 @@ package test
 // 如果出现了和maj不同的元素，说明maj可被抵消一次，count就自减一次，
 // 如果count减为0，也就说明maj元素已经被抵消完了，maj元素也不可能是出现次数超过n/2次的元素，
 // 因此就更新maj。显然，maj初始化可以赋予任何值，count初始化应当为0，程序如下：
-func majorityElement(nums []int) int {
+func vote2Elements(nums []int) int {
     major := 0
     count := 0
 
@@ -26,4 +34,52 @@ func majorityElement(nums []int) int {
     }
     
     return major
+}
+
+// 找出数组中出现次数超过n/3的元素
+func vote3Elements(nums []int) []int {
+    major1 := 0
+    major2 := 0
+    count1 := 0
+    count2 := 0
+
+    for _, num := range nums {
+        //如果出现两个元素出现次数同时抵消为0的情况，只选取一个标量赋值
+        if count1 == 0 {
+            major1 = num
+        } else if count2 == 0 {
+            major2 = num
+        }
+
+        // 三个数均不相等的时候，同时抵消
+        if major1 == num {
+            count1++
+        }else if major2 == num {
+            count2++
+        }else{
+            count1--
+            count2--
+        }
+    }
+
+    // 如果最后两个标量均为抵消为0，则说明两个元素都出现了三分之一次以上
+    if count1 != 0 && count2 != 0 {
+        return []int{major1, major2}
+    } else if count1 != 0 && count2 == 0 {
+        return []int{major1}
+    } else {
+        return []int{major2}
+    }
+}
+
+func TestVote2(t *testing.T){
+    s1 := []int{2,2,2,2,2,2,3,3,5,6}
+    result := vote2Elements(s1)
+    fmt.Println(result)
+}
+
+func TestVote3(t *testing.T){
+    s1 := []int{2,2,2,2,3,3,3,3,3,6}
+    result := vote3Elements(s1)
+    fmt.Println(result)
 }
